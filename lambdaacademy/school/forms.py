d1022f10768626b9b58388b3357a101ae9774124
Uneeb Agha <uneeb.i.am@gmail.com>
2015-07-07 18:14:04 +0500

    fixed form

--- lambdaacademy/school/forms.py
+++ lambdaacademy/school/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import Module
 
 time_widget = forms.widgets.TimeInput(attrs={'class': 'time-pick'})
-valid_time_formats = ['%H:%M']
+# valid_time_formats = ['%H:%M']
 
 class RegisterForm(forms.Form):
 
@@ -11,6 +11,6 @@ class RegisterForm(forms.Form):
 	f_name = forms.CharField(label = 'First Name', max_length=20)
 	l_name = forms.CharField(label = 'Last Name', max_length=20)
 	email = forms.EmailField(label = 'Email Address')
-	contact_number = forms.CharField(label = 'Contact', max_length=10, required=False)
-	best_suited_time = forms.TimeField(widget=time_widget, help_text='ex: 10:30AM', input_formats=valid_time_formats, label = 'Preferred Time', required=False)
+	contact_number = forms.CharField(label = 'Contact', max_length=15, required=False)
+	best_suited_time = forms.TimeField(widget=time_widget, help_text='ex: 10:30AM', label = 'Preferred Time', required=False)
 	comment = forms.CharField (label = 'Extra information', widget=forms.Textarea, required=False)
